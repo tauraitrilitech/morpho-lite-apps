bce9ddd9a9b6ad20312706673de7b7b353870396
Hayden Shively <hayden@morpho.xyz>
2025-02-10 15:56:02 -0600

    chore: add sidebar UI components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import { Button } from "@/components/ui/button";
 import {
   createConfig,
   fallback,
@@ -12,6 +8,7 @@ import {
 } from "wagmi";
 import { base, mainnet } from "viem/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import DashboardPage from "./app/dashboard/page";
 
 const wagmiConfig = createConfig({
   chains: [mainnet, base],
@@ -33,31 +30,10 @@ const wagmiConfig = createConfig({
 const queryClient = new QueryClient();
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
-        <div>
-          <a href="https://vite.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <Button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </Button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
+        <DashboardPage />
       </QueryClientProvider>
     </WagmiProvider>
   );
