326c25f467f7387f3318072af61ff7961e00d304
Hayden Shively <hayden@morpho.xyz>
2025-02-18 13:51:54 -0600

    fix: base addresses

--- src/components/constants.tsx
+++ src/components/constants.tsx
@@ -20,6 +20,20 @@ export const DEPLOYMENTS: Deployments = {
       fromBlock: 21439510n,
     },
   },
+  [base.id]: {
+    Morpho: {
+      address: "0xBBBBBbbBBb9cC5e90e3b3Af64bdAF62C37EEFFCb",
+      fromBlock: 13977148n,
+    },
+    MetaMorphoFactory: {
+      address: "0xA9c3D3a366466Fa809d1Ae982Fb2c46E5fC41101",
+      fromBlock: 13978134n,
+    },
+    MetaMorphoV1_1Factory: {
+      address: "0xFf62A7c278C62eD665133147129245053Bbf5918",
+      fromBlock: 23928808n,
+    },
+  },
 };
 
 export const CORE_DEPLOYMENTS = new Set<keyof Deployments>([mainnet.id, base.id]);
