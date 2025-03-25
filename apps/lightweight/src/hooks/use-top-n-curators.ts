684f33f27db8d7d708b6147f4576467af48aa058
Hayden Shively <17186559+haydenshively@users.noreply.github.com>
2025-03-25 16:41:50 -0500

    feat: top 5 curators hook (#16)

--- apps/lightweight/src/hooks/use-top-n-curators.ts
+++ apps/lightweight/src/hooks/use-top-n-curators.ts
@@ -1,31 +1,124 @@
-import { useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { useCallback, useMemo } from "react";
 import { useClient as useUrqlClient } from "urql";
 
 import { graphql } from "@/graphql/graphql";
 
-export function useTopNCurators() {
-  const curatorsQuery = graphql(`
-    query ExampleQuery {
-      curators {
-        items {
-          addresses {
-            address
-            chainId
+// NOTE: `state` is currently null; it's a work in progress on API side.
+// Once that's done we can use this (with an orderBy) to get top N by AUM.
+const curatorsQuery = graphql(`
+  query CuratorsQuery($first: Int, $verified: Boolean) {
+    curators(first: $first, where: { verified: $verified }) {
+      items {
+        addresses {
+          address
+          chainId
+        }
+        id
+        image
+        name
+        state {
+          aum
+          curatorId
+        }
+        url
+      }
+    }
+  }
+`);
+
+// NOTE: `curators` is marked as work in progress, but `state` seems to actually populate correctly,
+// so we're using this for now.
+const vaultsQuery = graphql(`
+  query VaultsQuery($first: Int, $chainIds: [Int!]) {
+    vaults(where: { whitelisted: true, chainId_in: $chainIds }, orderBy: TotalAssetsUsd, first: $first) {
+      items {
+        chain {
+          id
+        }
+        state {
+          curators {
+            id
+            state {
+              aum
+              curatorId
+            }
+            verified
           }
         }
       }
     }
-  `);
+  }
+`);
 
+export function useTopNCurators({
+  n,
+  verifiedOnly,
+  chainIds,
+  staleTime = 6 * 60 * 60 * 1_000,
+}: {
+  n: number;
+  verifiedOnly?: boolean;
+  chainIds: number[];
+  staleTime?: number;
+}) {
   const urqlClient = useUrqlClient();
 
-  const { data } = useQuery({
-    queryKey: ["testing"],
+  const { data: topNCuratorIds } = useQuery({
+    queryKey: ["useTopNCurators", "vaultsQuery", chainIds, verifiedOnly],
     queryFn: async () => {
-      const { data } = await urqlClient.query(curatorsQuery, {});
-      return data?.curators;
+      const { data, error } = await urqlClient.query(vaultsQuery, { first: 30, chainIds });
+      if (error || data === undefined || data.vaults.items == null) {
+        throw error ?? new Error("vaultsQuery GraphQL data was undefined or null.");
+      }
+
+      const vaultCuratorArrs = data.vaults.items.map((item) => item.state?.curators ?? []);
+      const curatorsSet = new Set<string>();
+      const curatorsArr: (typeof vaultCuratorArrs)[number] = [];
+
+      for (const vaultCuratorArr of vaultCuratorArrs) {
+        for (const curator of vaultCuratorArr) {
+          if (curator.state == null || curatorsSet.has(curator.id) || (verifiedOnly && !curator.verified)) continue;
+
+          curatorsSet.add(curator.id);
+          curatorsArr.push(curator);
+        }
+      }
+
+      // Sort by AUM, highest to lowest
+      curatorsArr.sort((a, b) => b.state!.aum - a.state!.aum);
+      return curatorsArr;
     },
+    select: useCallback(
+      (data: { id: string }[]) => {
+        return new Set(data.slice(0, n).map((curator) => curator.id));
+      },
+      [n],
+    ),
+    staleTime,
+    gcTime: Infinity,
+    placeholderData: keepPreviousData,
+    notifyOnChangeProps: ["data"],
   });
 
-  console.log("curators", data);
+  const { data: curators } = useQuery({
+    queryKey: ["useTopNCurators", "curatorsQuery", verifiedOnly],
+    queryFn: async () => {
+      const { data, error } = await urqlClient.query(curatorsQuery, { first: 100, verified: verifiedOnly });
+      if (error || data === undefined || data.curators.items == null) {
+        throw error ?? new Error("curatorsQuery GraphQL data was undefined or null.");
+      }
+
+      return data.curators.items;
+    },
+    staleTime,
+    gcTime: Infinity,
+    placeholderData: keepPreviousData,
+    notifyOnChangeProps: ["data"],
+  });
+
+  return useMemo(() => {
+    if (topNCuratorIds === undefined || curators === undefined) return [];
+    return curators.filter((curator) => topNCuratorIds.has(curator.id));
+  }, [topNCuratorIds, curators]);
 }
