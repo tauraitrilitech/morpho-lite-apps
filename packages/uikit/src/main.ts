a522f8393df0aa39932fdd19dc3c27d191b31c59
Hayden Shively <hayden@morpho.xyz>
2025-04-16 00:06:17 -0500

    chore(lite): update svg assets

--- packages/uikit/src/main.ts
+++ packages/uikit/src/main.ts
@@ -1,3 +1,6 @@
 // NOTE: Any SVGs we wish to export from this library MUST BE referenced explicitly with "?no-inline"
+import morphoHorizontalLiteSvg from "./assets/morpho-horizontal-lite.svg?no-inline";
+import morphoHorizontalSvg from "./assets/morpho-horizontal.svg?no-inline";
 import morphoSvg from "./assets/morpho.svg?no-inline";
-export { morphoSvg };
+import poweredByMorphoSvg from "./assets/powered-by-morpho.svg?no-inline";
+export { morphoHorizontalLiteSvg, morphoHorizontalSvg, morphoSvg, poweredByMorphoSvg };
