d42acf8d6aeb4bb2e19c8490b8394fb307ac6ade
Hayden Shively <hayden@morpho.xyz>
2025-04-16 00:03:46 -0500

    chore(ci): fix missing matchMedia with mock

--- apps/lightweight/test/setup.ts
+++ apps/lightweight/test/setup.ts
@@ -8,6 +8,20 @@ global.ResizeObserver = vi.fn().mockImplementation(() => ({
   disconnect: vi.fn(),
 }));
 
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: vi.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // deprecated
+    removeListener: vi.fn(), // deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});
+
 window.HTMLElement.prototype.scrollIntoView = vi.fn();
 window.HTMLElement.prototype.hasPointerCapture = vi.fn();
 window.HTMLElement.prototype.releasePointerCapture = vi.fn();
