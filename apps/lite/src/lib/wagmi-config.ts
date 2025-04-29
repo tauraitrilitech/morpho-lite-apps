1e1935c176bb2bdbbbcd86674c1543e3a11077a4
Hayden Shively <hayden@morpho.xyz>
2025-04-29 01:53:42 -0500

    chore(lite): temporarily disable plume

--- apps/lite/src/lib/wagmi-config.ts
+++ apps/lite/src/lib/wagmi-config.ts
@@ -68,7 +68,7 @@ const chains = [
   // corn,
   // modeMainnet,
   // hemi,
-  plumeMainnet,
+  // plumeMainnet,
 ] as const;
 
 const transports: { [K in (typeof chains)[number]["id"]]: Transport } & { [k: number]: Transport } = {
