37e8dfb556da33d4110c0124a0bcc7d4a625bc36
Hayden Shively <hayden@morpho.xyz>
2025-05-02 11:13:11 -0500

    fix(lite): polyfills for old browsers and new ones missing array.iterator

--- apps/lite/vite.config.ts
+++ apps/lite/vite.config.ts
@@ -2,13 +2,19 @@
 import path from "path";
 
 import tailwindcss from "@tailwindcss/vite";
+import legacy from "@vitejs/plugin-legacy";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svgr(), tailwindcss(), react()],
+  plugins: [
+    svgr(),
+    tailwindcss(),
+    react(),
+    legacy({ targets: ["defaults", "not IE 11"], modernPolyfills: ["es.array.iterator"] }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
