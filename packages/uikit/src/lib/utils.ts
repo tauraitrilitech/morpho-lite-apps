e854d44d1f8d3538741f752e6a1c7c0e4a858695
Hayden Shively <17186559+haydenshively@users.noreply.github.com>
2025-06-26 13:17:24 -0400

    chore(uikit): add lisk token logos (#57)

--- packages/uikit/src/lib/utils.ts
+++ packages/uikit/src/lib/utils.ts
@@ -85,6 +85,15 @@ export function getTokenSymbolURI(symbol: string | undefined): Token["imageSrc"]
   } else if (symbol === "WLD") {
     // TODO: remove once it's included on Morpho CDN
     return "https://storage.googleapis.com/merkl-static-assets/tokens/WLD.jpeg";
+  } else if (symbol === "LSK") {
+    // TODO: remove once it's included on Morpho CDN
+    return "https://s2.coinmarketcap.com/static/img/coins/64x64/1214.png";
+  } else if (symbol === "M-BTC") {
+    // TODO: remove once it's included on Morpho CDN
+    return "https://s2.coinmarketcap.com/static/img/coins/64x64/34686.png";
+  } else if (symbol === "USD₮0") {
+    // TODO: remove once it's included on Morpho CDN
+    return "https://assets.coingecko.com/coins/images/53705/standard/usdt0.jpg";
   }
   return `https://cdn.morpho.org/assets/logos/${encodeURIComponent((symbol ?? "").toLowerCase())}.svg`;
 }
