bdde2ed4cbd033cbd23250c6e0e7656f6aa743e7
Hayden Shively <hayden@morpho.xyz>
2025-07-17 01:48:17 -0400

    chore(lite): update docs links

--- apps/lite/src/lib/constants.tsx
+++ apps/lite/src/lib/constants.tsx
@@ -19,9 +19,9 @@ export const DEFAULT_CHAIN = plumeMainnet;
 export const TRANSACTION_DATA_SUFFIX = "0x117E"; // (L I T E)
 
 export const TERMS_OF_USE = "https://cdn.morpho.org/documents/Morpho_Terms_of_Use.pdf";
-export const RISKS_DOCUMENTATION = "https://docs.morpho.org/overview/resources/risks/";
-export const ADDRESSES_DOCUMENTATION = "https://docs.morpho.org/overview/resources/addresses/";
-export const SHARED_LIQUIDITY_DOCUMENTATION = "https://docs.morpho.org/overview/concepts/public-allocator/";
+export const RISKS_DOCUMENTATION = "https://docs.morpho.org/learn/resources/risks/";
+export const ADDRESSES_DOCUMENTATION = "https://docs.morpho.org/getting-started/resources/addresses/";
+export const SHARED_LIQUIDITY_DOCUMENTATION = "https://docs.morpho.org/build/borrow/concepts/public-allocator";
 
 export const BANNERS: Record<keyof Deployments, { color: string; text: ReactNode }> = {
   [plumeMainnet.id]: {
